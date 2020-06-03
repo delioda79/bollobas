d496548c996a1194be76e50eeb9510a28791f202
Filippos Pavlopoulos <philpavlop@gmail.com>
2020-06-03 11:20:39 +0300

    Add json tags (#67)

--- internal/semovi/rest/http/view/view.go
+++ internal/semovi/rest/http/view/view.go
@@ -3,52 +3,52 @@ package view
 // AggregatedTrips is the transport layer view of the Aggregated Trips.
 type AggregatedTrips struct {
 	ID                     int64    `json:"id"`
-	Date                   string   `transl:"date" json:"fecha"`
-	SupplierID             *string  `transl:"supplier_id" json:"id_proveedor"`
-	TotalRides             *int     `transl:"total_rides" json:"tot_viajes"`
-	TotalVehicleRides      *int     `transl:"total_vehicle_rides" json:"tot_veh_viaje"`
-	TotalAvailableVehicles *int     `transl:"total_available_vehicles" json:"tot_veh_disp"`
-	TotalDistTraveled      *float64 `transl:"total_dist_traveled" json:"dist_pasajero"`
-	PassingTime            *float64 `transl:"passing_time" json:"tiempo_pasajero"`
-	RequestTime            *float64 `transl:"request_time" json:"tiempo_solicitud"`
-	EmptyTime              *float64 `transl:"empty_time" json:"tiempo_vacio"`
-	EodMultiplier          *float64 `transl:"eod_multiplier" json:"multiplicador_eod"`
-	Accessibility          *float64 `transl:"accessibility" json:"accesibilidad"`
-	FemaleOperator         *float64 `transl:"female_operator" json:"operador_mujer"`
-	EodStart               *int     `transl:"eod_start" json:"inicio_eod"`
-	EodEnd                 *int     `transl:"eod_end" json:"fin_eod"`
-	EodPassDist            *float64 `transl:"eod_pass_dist" json:"dist_pasajero_eod"`
-	EodPassTime            *int     `transl:"eod_pass_time" json:"tiempo_pasajero_eod"`
-	RequestDist            *float64 `transl:"request_dist" json:"dist_solicitud"`
-	EmptyDist              *float64 `transl:"empty_dist" json:"dist_vacío"`
-	EodRequestDist         *float64 `transl:"eod_request_dist" json:"dist_solicitud_eod"`
-	EodRequestTime         *float64 `transl:"eod_request_time" json:"tiempo_solicitud_eod"`
-	EodEmptyDist           *float64 `transl:"eod_empty_dist" json:"dist_vacio_eod"`
-	EodEmptyTime           *float64 `transl:"eod_empty_time" json:"tiempo_vacio_eod"`
+	Date                   string   `json:"fecha"`
+	SupplierID             *string  `json:"id_proveedor"`
+	TotalRides             *int     `json:"tot_viajes"`
+	TotalVehicleRides      *int     `json:"tot_veh_viaje"`
+	TotalAvailableVehicles *int     `json:"tot_veh_disp"`
+	TotalDistTraveled      *float64 `json:"dist_pasajero"`
+	PassingTime            *float64 `json:"tiempo_pasajero"`
+	RequestTime            *float64 `json:"tiempo_solicitud"`
+	EmptyTime              *float64 `json:"tiempo_vacio"`
+	EodMultiplier          *float64 `json:"multiplicador_eod"`
+	Accessibility          *float64 `json:"accesibilidad"`
+	FemaleOperator         *float64 `json:"operador_mujer"`
+	EodStart               *int     `json:"inicio_eod"`
+	EodEnd                 *int     `json:"fin_eod"`
+	EodPassDist            *float64 `json:"dist_pasajero_eod"`
+	EodPassTime            *int     `json:"tiempo_pasajero_eod"`
+	RequestDist            *float64 `json:"dist_solicitud"`
+	EmptyDist              *float64 `json:"dist_vacío"`
+	EodRequestDist         *float64 `json:"dist_solicitud_eod"`
+	EodRequestTime         *float64 `json:"tiempo_solicitud_eod"`
+	EodEmptyDist           *float64 `json:"dist_vacio_eod"`
+	EodEmptyTime           *float64 `json:"tiempo_vacio_eod"`
 }
 
 // OperatorStats is the view model of the Operator Stats.
 type OperatorStats struct {
-	ID             int64
-	Date           string
-	OperatorID     *string
-	Gender         *int
-	CompletedTrips *int
-	DaysSince      *int
-	AgeRange       *string
-	HoursConnected *string
-	TripHours      *string
-	TotRevenue     *string
+	ID             int64   `json:"id"`
+	Date           string  `json:"fecha_produccion"`
+	OperatorID     *string `json:"id_operador"`
+	Gender         *int    `json:"genero"`
+	CompletedTrips *int    `json:"cant_viajes"`
+	DaysSince      *int    `json:"tiempo_registro"`
+	AgeRange       *string `json:"edad"`
+	HoursConnected *string `json:"horas_conectado"`
+	TripHours      *string `json:"horas_viaje"`
+	TotRevenue     *string `json:"ingreso_totales"`
 }
 
 // TrafficIncident is the view model of the Traffic Incident.
 type TrafficIncident struct {
-	ID             int64
-	Date           string
-	Type           *int
-	Plates         *string
-	Licence        *string
-	TravelDistance *string
-	TravelTime     *string
-	Coordinates    *string
+	ID             int64   `json:"id"`
+	Date           string  `json:"tiempo_hecho"`
+	Type           *int    `json:"hecho_trans"`
+	Plates         *string `json:"placa"`
+	Licence        *string `json:"licencia"`
+	TravelDistance *string `json:"distancia_viaje"`
+	TravelTime     *string `json:"tiempo_viaje"`
+	Coordinates    *string `json:"ubicación"`
 }
